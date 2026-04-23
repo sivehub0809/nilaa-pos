2fd8f2e18cb4064b323232bbb444ed43c0df92e9
sivehub0809 <sivehub0809@users.noreply.github.com>
2026-04-23 17:35:15 +0700

    Add real Firebase project configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyCXQPF-JKFJajP3jNG7ESvaA2z4hfOWEQw",
+  authDomain: "nilaa-os.firebaseapp.com",
+  projectId: "nilaa-os",
+  storageBucket: "nilaa-os.firebasestorage.app",
+  messagingSenderId: "964294265808",
+  appId: "1:964294265808:web:99aba860471dab219b87d5",
+  measurementId: "G-KY5RK5QYW4",
   functionsRegion: "asia-southeast1"
 };
 
